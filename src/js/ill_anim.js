0ab5b8c11dbea18dcf8ff9c037bf213dcf76c46c
Pavel Proyavin <pavel@dorohovdesign.com>
2019-04-11 17:56:31 +0300

    Changelog from 110419

--- src/js/ill_anim.js
+++ src/js/ill_anim.js
@@ -14,6 +14,16 @@
             })
         }
 
+        function getRandomTransform(x_min, x_max, y_min, y_max, scale_min, scale_max, rotate_min, rotate_max) {
+
+            var x = getRandomArbitrary(x_min, x_max)
+            var y = getRandomArbitrary(y_min, y_max)
+            var scale = getRandomArbitrary(scale_min, scale_max)
+            var rotate = getRandomArbitrary(rotate_min, rotate_max)
+
+            return 'translate3d(' + x + 'px, ' + y + 'px, 0) scale(' + scale + ') rotate(' + rotate + 'deg)'
+        }
+
 
         $('.banner').mousemove(function(e) {
             setPosititionForDavid(e)
@@ -25,7 +35,7 @@
 
         $('.animationSvg path').each(function() {
 
-            var thisTransform = 'translate3d(' + getRandomArbitrary(-1500, 1500) + 'px, ' + getRandomArbitrary(-1500, 1500) + 'px, 0) scale(' + getRandomArbitrary(0, 1) + ') rotate(' + getRandomArbitrary(-400, 400) + 'deg)'
+            var thisTransform = getRandomTransform(-1500, 1500, -1500, 1500, 0, 1, -400, 400)
 
             $(this).css({
                 '-webkit-transform' : thisTransform,
@@ -39,6 +49,28 @@
             // console.log(thisTransform)
         })
 
+        /*
+        $('.teamIll').hover(function() {
+            $(this).children('path').each(function() {
+                var thisTransform = getRandomTransform(-10, 10, -10, 10, 1, 1, 0, 0)
+                $(this).css({
+                    '-webkit-transform' : thisTransform,
+                    '-moz-transform'    : thisTransform,
+                    '-ms-transform'     : thisTransform,
+                    '-o-transform'      : thisTransform,
+                    'transform': thisTransform,
+                    "transition": getRandomArbitrary(.5, 3) + 's all ease'
+                })
+            })
+        }, function() {
+            $(this).children('path').each(function() {
+                $(this).css({
+                    'transform': 'translate3d(0, 0, 0)'
+                })
+            })
+        })
+        */
+
         setTimeout(function() {
             $('.animationSvg path').each(function() {
                 $(this).css({
@@ -51,6 +83,18 @@
         ill_s.on('change', function(e, transforms){
             console.log(e, transforms)
         });
+
+        var jobsTimer
+
+        $('.jobsImageTarget').hover(function() {
+            clearTimeout(jobsTimer)
+            jobsTimer = setTimeout(function() {
+                $('.jobsImageHidden').addClass('jobsImageHidden-visible')
+            }, 1000)
+        }, function() {
+            clearTimeout(jobsTimer)
+            $('.jobsImageHidden').removeClass('jobsImageHidden-visible')
+        }) 
 
     })
 })(jQuery);
---
+++ src/js/presentation_carousel.js
@@ -0,0 +1,11 @@
+(function($) {
+    "use strict"
+    $(function() {
+
+        $('.presentation__carousel__items').slick({
+            prevArrow: '.presentation__carousel__arrow.is--prev',
+            nextArrow: '.presentation__carousel__arrow.is--next'
+        });
+
+    })
+})(jQuery);
