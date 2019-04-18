b99512d20a9e4e36eb5827879f314a1a115276f8
Pavel Proyavin <pavel@dorohovdesign.com>
2019-04-18 15:22:19 +0300

    Release

---
+++ src/js/team.js
@@ -0,0 +1,30 @@
+(function($) {
+    "use strict"
+    $(function() {
+
+        var prof, animationTimer
+
+        $('.teamPic').hover(function(e) {
+            clearTimeout(animationTimer)
+            var _this = $(this).attr('id')
+            prof = $(this).attr('id')
+            $('#teamHeading span').addClass('is--animate')
+
+            $('.about__team__content__cloud').addClass('is--active')
+            $('#teamHeading span').html($(this).data('profname'))
+            $('.about__team').addClass('is--' + prof)
+            $('.teamPic').each(function() {
+                if($(this).attr('id') != _this) {
+                    $(this).addClass('is--unhover')
+                }
+            })
+        }, function() {
+            $('.about__team').removeClass('is--' + prof)
+            $('.about__team__content__cloud').removeClass('is--active')
+            $('#teamHeading span').removeClass('is--animate')
+            $('.teamPic').removeClass('is--unhover')
+            $('#teamHeading span').html('Наша команда')
+        })
+
+    })
+})(jQuery);
