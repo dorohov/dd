f38ca3ab29766b3200159243fc03c2a1977036c1
Pavel Proyavin <pavel@dorohovdesign.com>
2019-05-07 17:25:00 +0300

    Fixes from 070519

--- src/js/pres.js
+++ src/js/pres.js
@@ -2,10 +2,14 @@
     "use strict"
     $(function() {
 
+
         var index = window.location.href.indexOf('/portfolio/');
+
         if(index > 0) {
-            $('.navbar').removeClass('is--transparent')
+            if(window.location.href.split('/portfolio/')[1].length > 1) {
+                $('.navbar').removeClass('is--transparent')
                         .addClass('is--dark')
+            }
         }
 
     })
