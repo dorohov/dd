5b784545fcfd9654ee7efdce4147ad551779d37c
Pavel Proyavin <pavel@dorohovdesign.com>
2019-06-20 11:02:58 +0300

    fixes in landing

--- src/js/landing_b.js
+++ src/js/landing_b.js
@@ -8,5 +8,25 @@
             adaptiveHeight: true
         });
 
+        setHeightInCarousel()
+
+        $(window).resize(function() {
+            setHeightInCarousel()
+        })
+
+        function setHeightInCarousel() {
+            var block = $('.bl-sx__item__block')
+            var windowWidth = $(window).outerWidth()
+            if(windowWidth <= 768) {
+                $('.bl-sx__item').css({
+                    minHeight: block.outerHeight() + 230
+                })
+            }else {
+                $('.bl-sx__item').css({
+                    minHeight: '100vh'
+                })
+            }
+        }
+
     })
 })(jQuery);
