2f34d29e471209a3dd24fa4748410e50a96ba943
Pavel Proyavin <pavel@dorohovdesign.com>
2019-07-08 10:38:44 +0300

    changed map

--- src/js/map.js
+++ src/js/map.js
@@ -1,41 +1,41 @@
-var map;
-        function initMap() {
-          map = new google.maps.Map(document.getElementById('map'), {
-            center: new google.maps.LatLng(52.974394, 36.059105),
-            zoom: 18,
-            disableDefaultUI: true,
-            styles: [
-                {
-                    "featureType": "all",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "invert_lightness": true
-                        },
-                        {
-                            "saturation": 10
-                        },
-                        {
-                            "lightness": 30
-                        },
-                        {
-                            "gamma": 0.5
-                        },
-                        {
-                            "hue": "#435158"
-                        }
-                    ]
-                }
-            ]
-          });
-          var icon = 'http://frontend.dev.dorohovdesign.ru/dd/img/marker.png';
+// var map;
+//         function initMap() {
+//           map = new google.maps.Map(document.getElementById('map'), {
+//             center: new google.maps.LatLng(52.974394, 36.059105),
+//             zoom: 18,
+//             disableDefaultUI: true,
+//             styles: [
+//                 {
+//                     "featureType": "all",
+//                     "elementType": "all",
+//                     "stylers": [
+//                         {
+//                             "invert_lightness": true
+//                         },
+//                         {
+//                             "saturation": 10
+//                         },
+//                         {
+//                             "lightness": 30
+//                         },
+//                         {
+//                             "gamma": 0.5
+//                         },
+//                         {
+//                             "hue": "#435158"
+//                         }
+//                     ]
+//                 }
+//             ]
+//           });
+//           var icon = 'http://frontend.dev.dorohovdesign.ru/dd/img/marker.png';
 
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(52.974394, 36.059105),
-                icon: icon,
-                map: map
-            });
-        }
+//             var marker = new google.maps.Marker({
+//                 position: new google.maps.LatLng(52.974394, 36.059105),
+//                 icon: icon,
+//                 map: map
+//             });
+//         }
 
 (function($) {
     "use strict"
